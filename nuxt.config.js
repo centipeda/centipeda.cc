e56afd1922531a1670e1f611a4ddb40373b01306
centipeda <joshua.c.guam@gmail.com>
2022-10-14 00:17:40 -0400

    remove trailing slash

--- nuxt.config.js
+++ nuxt.config.js
@@ -2,6 +2,10 @@ export default {
   // Target: https://go.nuxtjs.dev/config-target
   target: 'static',
 
+  router: {
+    trailingSlash: false,
+  },
+
   // Global page headers: https://go.nuxtjs.dev/config-head
   head: {
     title: 'centipeda.cc',
